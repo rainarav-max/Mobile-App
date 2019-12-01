000b7c89ffa9afcb6922cd8d29449c38a37a3c17
Raina <rainarrav@sheridancollege.ca>
2019-12-01 13:41:40 -0500

    Final Code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Assignment3';
+  // @ts-ignore
+  // @ts-ignore
+  // tslint:disable-next-line:max-line-length
+  studentRecords: object[] = [ {firstName: 'Sarabjit', lastName: 'Raina', programId: 23, programYear: 2, programCoop: 'yes', programInternship: 'yes' },
+  {firstName: 'Ravneet', lastName: 'Raina', programId: 28, programYear: 3, programCoop: 'no', programInternship: 'no' },
+    {firstName: 'Tavleen', lastName: 'Raina', programId: 34, programYear: 4, programCoop: 'yes', programInternship: 'no' },
+    {firstName: 'Abhi', lastName: 'Awal', programId: 28, programYear: 2, programCoop: 'no', programInternship: 'no' }
+    ];
+  public title = 'Getting Details of a Person';
+  public student: object;
+  constructor() {
+    this.selectStudent(0);
+  }
+  public selectStudent(index) {
+    this.student = this.studentRecords[index];
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {FooterComponent} from './footer.component';
+
+// @ts-ignore
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule
